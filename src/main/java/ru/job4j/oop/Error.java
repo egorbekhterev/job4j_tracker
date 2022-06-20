c6f99826eeaf9efd5afbc709dab08c2b98aebf12
Paranoj <e.behterev@yandex.ru>
2022-06-20 17:17:33 +0700

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+
+    }
+
+    public void printError() {
+        System.out.println("Активность: " + active);
+        System.out.println("Состояние: " + status);
+        System.out.println("Код ошибки: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 2, "Error 404.");
+        error.printError();
+        Error errorDefault = new Error();
+        errorDefault.printError();
+    }
+}
