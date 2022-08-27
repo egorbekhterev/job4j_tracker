83c68dc6b845cbd8d3c47e404f127b0bbec6720c
Paranoj <e.behterev@yandex.ru>
2022-08-27 21:00:13 +0700

    1. Singleton [#94606 #335781]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,7 +1,8 @@
 package ru.job4j.tracker;
 
-public class SingleTracker {
+public final class SingleTracker {
     private static SingleTracker instance = null;
+    private Tracker tracker = new Tracker();
 
     private SingleTracker() {
     }
@@ -12,8 +13,6 @@ public class SingleTracker {
         }
         return instance;
     }
-
-    private Tracker tracker = new Tracker();
 
     public Item add(Item item) {
         return tracker.add(item);
