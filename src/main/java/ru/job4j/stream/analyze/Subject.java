a9a3c2c29b17c914c89b53e303097de100fc4877
Paranoj <e.behterev@yandex.ru>
2022-10-23 14:40:46 +0700

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/analyze/Subject.java
@@ -0,0 +1,4 @@
+package ru.job4j.stream.analyze;
+
+public record Subject(String name, int score) {
+}
---
+++ src/main/java/ru/job4j/stream/analyze/Tuple.java
@@ -0,0 +1,4 @@
+package ru.job4j.stream.analyze;
+
+public record Tuple(String name, double score) {
+}
